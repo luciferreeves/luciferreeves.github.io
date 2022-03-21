63d3752a4fe148af6ac7d81072e69a362a81828b
Bobby <bobbyskhs@gmail.com>
2022-03-21 00:42:29 -0400

    Server side rendering of repos page

--- routes/index.js
+++ routes/index.js
@@ -3,13 +3,13 @@ const express = require("express");
 const router = express.Router();
 // Import the routes
 const admin = require("./admin");
-const api = require("./api");
+const repositories = require("./repositories");
 const blog = require("./blog");
 const posts = require("./posts");
 // Set the routes
 router.use("/admin", admin);
-router.use("/api", api);
 router.use("/api/blog", blog);
+router.use("/", repositories);
 router.use("/", posts);
 
 // Create the routes
@@ -18,9 +18,6 @@ router.get("/", (req, res) => {
 });
 router.get("/about", (req, res) => {
   res.render("about.html");
-});
-router.get("/repos", (req, res) => {
-  res.render("repositories.html");
 });
 
 
